a33d077030eb23de32dc10c0062b1f020b05fe74
AnuGlimmersky <anuks3000@gmail.com>
2018-02-06 17:11:44 +0530

    DT cal metadata tool kit added. direct trust cal code changed

---
+++ model/MetaDataToolKit.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "NodeEntry.h"
+
+namespace ns3
+{
+namespace aodv
+{
+
+class MetaDataToolKit
+{
+private:
+	int rreq;
+	int rply;
+	int hello;
+	int err;
+	int noOfDataPacketsForward;
+	int noOfDataPacketsReceived;
+	int noOfControlPackets;
+	int noOfDataPackets;
+public:
+	MetaDataToolKit();
+	void incRREQ();
+	void incRPLY();
+	void incERR();
+	void incHELLO();
+	void incNDF();
+	void incNDR();
+	int getHELLO();
+	int getERR();
+	int getRREQ();
+	int getRPLY();
+	int getNDF();
+	int getNDR();
+        NodeEntry* getAllNeiNodes();
+
+
+	~MetaDataToolKit();
+};
+
+}
+}
